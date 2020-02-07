7bea52d3025e8397f1a0e6f8fc62c1f5b10246ab
Bryn <thenazboz@gmail.com>
2020-02-07 11:23:28 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,20 +1,10 @@
 class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
-  @@all = []
-  def initialize(*args)
-    @name = args[0][:name]
-    @birthday = args[0][:birthday]
-    @hair_color = args[0][:hair_color]
-    @eye_color = args[0][:eye_color]
-    @height = args[0][:height]
-    @weight = args[0][:weight]
-    @handed = args[0][:handed]
-    @complexion = args[0][:complexion]
-    @t_shirt_size = args[0][:t_shirt_size]
-    @wrist_size = args[0][:wrist_size]
-    @glove_size = args[0][:glove_size]
-    @pant_length = args[0][:pant_length]
-    @pant_width = args[0][:pant_width]
-    @@all << self
+  def initialize(attributes=nil)
+    if attributes
+      attributes.each do |k,v|
+        self.send("#{k}=", v)
+      end
+    end
   end
 end
